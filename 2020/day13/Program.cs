0c29fe2bc030239ef4204e466ea60cd248ab86b4
David van der Hoop <vanderhoop@evident.nl>
2021-12-01 10:56:06 +0100

    last bit of 2020 (forgot to commit last year) and day 1 of 2021

---
+++ 2020/day13/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace day13
+{
+    class Program
+    {
+        static int Solve1(string input, bool verbose = false){
+            var earliestPossibleBus = int.Parse(input.Split(Environment.NewLine).First());
+            var bussesInService = input.Split(Environment.NewLine)
+                                        .Skip(1)
+                                        .First()
+                                        .Split(',')
+                                        .Where(x => x != "x")
+                                        .Select(int.Parse);
+            var orderedByEarliest = bussesInService.Select(x => new { Bus = x, Closest = x - earliestPossibleBus % x })
+                                                    .OrderBy(x => x.Closest);
+            if(verbose){
+                foreach(var x in orderedByEarliest) {
+                    Console.WriteLine($"Bus: {x.Bus}, {x.Closest}");
+                }
+            }
+            var earliest = orderedByEarliest.First();
+            return earliest.Bus * earliest.Closest;
+        }
+        private static IEnumerable<bool> Infinite()  
+        {  
+            while (true)  
+            {  
+                yield return true;  
+            }  
+        }
+        static long Solve2(string input, bool verbose = false){
+            var answer = 0L;
+
+            var list = input.Split(',')
+                            .Select((x, i) => new { Index = i, Bus = x == "x" ? 0 : int.Parse(x) })
+                            .Where(x => x.Bus > 0)
+                            .ToArray();
+
+            if(verbose) {
+                Console.WriteLine(list.Select(x => $"{x.Index} mod({x.Bus})" ).Aggregate((x, y) => $"{x},{y}"));
+            }
+
+            long increment = list.First().Bus;
+            var currentBusIndex = 1;
+            var currentbus = list[currentBusIndex];
+            while(true) {
+                //if(verbose) Console.WriteLine($"{currentbus.Bus} - {answer} % {currentbus.Bus} = {currentbus.Bus - answer % currentbus.Bus} = {currentbus.Index}");
+                if(currentbus.Index == currentbus.Bus - answer % currentbus.Bus
+                    || currentbus.Index % currentbus.Bus == currentbus.Bus - answer % currentbus.Bus) {
+                    currentBusIndex++;
+                    if(currentBusIndex == list.Count()) break;
+                    increment *= currentbus.Bus;
+                    currentbus = list[currentBusIndex];
+                    if(verbose) Console.WriteLine($"New increment: {list.Take(currentBusIndex).Select(x => x.Bus.ToString()).Aggregate((x, y) => $"{x}*{y}")} = {increment}");
+                }
+
+                answer += increment;
+                //if(answer > 4000) break;
+            }
+            return answer;
+        }
+        static long Solve2BruteForce(string input, long start = 0, bool verbose = false){
+            var list = input.Split(',')
+                            .Select((x, i) => new { Index = i, BusLine = x })
+                            .Where(x => x.BusLine != "x")
+                            .Select(x => new { Index = x.Index, BusLine = int.Parse(x.BusLine)});
+            long answer = start;
+            var tracker = list.FirstOrDefault();
+            Parallel.ForEach(Infinite(), new ParallelOptions() { MaxDegreeOfParallelism = 8 }, (line, state, index) =>
+            {
+                var first = list.FirstOrDefault(x => {
+                        switch(x.Index) {
+                            case 0:
+                                return x.Index != index % x.BusLine;
+                            default:
+                                return x.Index != x.BusLine - index % x.BusLine;
+                        }
+                    });
+                if(first == null) {
+                    answer = index;
+                    state.Break();
+                }
+            });
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Answer part 1 (test input): {Solve1(testInput, true)}");
+            Console.WriteLine($"Answer part 1: {Solve1(input)}");
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Answer part 2 (test input): {Solve2("17,x,13,19", false)}");
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Answer part 2 (test input): {Solve2("7,13,x,x,59,x,31,19", false)}");
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+            
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Answer part 2 (test input): {Solve2("67,7,59,61", false)}");
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Answer part 2 (test input): {Solve2("67,x,7,59,61")}");
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Answer part 2 (test input): {Solve2("67,7,x,59,61")}");
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Answer part 2 (test input): {Solve2("1789,37,47,1889", false)}");
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+            
+            sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine($"Answer part 2 (test input): {Solve2(input.Split(Environment.NewLine).Last(), true)}");
+            sw.Stop();
+            Console.WriteLine($"Time: {sw.Elapsed}");
+        }
+
+        private static string testInput = @"939
+        7,13,59,31,19";
+        private static string input = @"1005595
+41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,557,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,x,x,419,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19"; 
+    }
+}
---
+++ 2020/day17/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace day17
+{
+    class Grid {
+        public int Width { get; }
+        public int Height { get; }
+        public int Depth { get; }
+        public Cell[,,] Cells { get; }
+
+        public Grid(int width, int height, int depth){
+            Width = width;
+            Height = height;
+            Depth = depth;
+            Cells = new Cell[depth,height,width];
+            forEachCell((x,y,z) => {
+                Cells[x,y,z] = new Cell(x,y,z);
+                return 1;
+            });
+        }
+        public static Grid InitializeFromInput(string input) {
+            var lines = input.Split(Environment.NewLine);
+            var gridSizeX = lines.First().Length * 10;
+            var gridSizeY = lines.Count() * 10;
+            var gridSizeZ = gridSizeX > gridSizeY ? gridSizeX : gridSizeY;
+            var result = new Grid(gridSizeX, gridSizeY, gridSizeZ);
+            var startZ = gridSizeZ / 2;
+            var startX = (gridSizeX / 2) - (lines.First().Length / 2);
+            var startY = (gridSizeY / 2) - (lines.Count() / 2);
+            var endY = startY + lines.Count();
+            var endX = startX + lines.First().Length;
+            for (int y = startY; y < endY; y++) {
+                for (int x = startX; x < endX; x++) {
+                    result.Cells[x,y,startZ].Alive = lines[y-startY][x-startX] == '#';
+                }
+            }
+            return result;
+        }
+        public int CountAlive(){
+            var result = 0;
+            forEachCell((x,y,z) => {
+                result += Cells[x,y,z].Alive ? 1 : 0;
+                return 0;
+            });
+            return result;
+        }
+        public void Run(int cycles = 1){
+            for(var i = 0; i < cycles; i++) {
+                forEachCell((x,y,z) => {
+                    Cells[x,y,z].Neighbours = countNeighbours(x,y,z);
+                    return 0;
+                });
+                forEachCell((x,y,z) => {
+                    if(Cells[x,y,z].Alive) {
+                        Cells[x,y,z].Alive = Cells[x,y,z].Neighbours == 2 || Cells[x,y,z].Neighbours == 3;
+                    } else {
+                        Cells[x,y,z].Alive = Cells[x,y,z].Neighbours == 3;
+                    }
+                    return 0;
+                });
+            }
+        }
+        public string Print(int z) {
+            var result = new StringBuilder();
+            for (int y = 0; y < Height; y++) {
+                for (int x = 0; x < Width; x++) {
+                    if(Cells[x,y,z].Alive) {
+                        result.Append('#');
+                    }else{
+                        result.Append('.');
+                    }
+                }
+                result.AppendLine();
+            }
+            return result.ToString();
+        }
+        private int countNeighbours(int x, int y, int z) {
+        int neighbours = 0;
+        for (int zi = z - 1; zi <= z + 1; zi++) {
+            if (zi < 0 || zi >= Depth) continue;
+            for (int yi = y - 1; yi <= y + 1; yi++) {
+                if (yi < 0 || yi >= Height) continue;
+                for (int xi = x - 1; xi <= x + 1; xi++) {
+                    if (xi < 0 || xi >= Width) continue;
+                    else if (xi == x && yi == y && zi == z) continue;
+                    else if (Cells[xi,yi,zi].Alive) neighbours++;
+                }
+            }
+        }
+        return neighbours;
+    }
+        private int forEachCell(Func<int, int, int, int> runnable) {
+            var sum = 0;
+            for (int z = 0; z < Depth; z++) {
+                for (int y = 0; y < Height; y++) {
+                    for (int x = 0; x < Width; x++) {
+                        sum += runnable(x, y, z);
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+
+    class Cell
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+        public int Neighbours { get; set; }
+        public bool Alive { get; set; }
+        public Cell(int x, int y, int z) {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grid = Grid.InitializeFromInput(@"...#...#
+#######.
+....###.
+.#..#...
+#.#.....
+.##.....
+#.####..
+#....##.");
+            Console.WriteLine(grid.Print(15));
+            grid.Run(6);
+            // Console.WriteLine(grid.Print(13));
+            // Console.WriteLine(grid.Print(14));
+            // Console.WriteLine(grid.Print(15));
+            // Console.WriteLine(grid.Print(16));
+            // Console.WriteLine(grid.Print(17));
+            Console.WriteLine($"Answer day 17, part 1: {grid.CountAlive()}");
+        }
+    }
+}
